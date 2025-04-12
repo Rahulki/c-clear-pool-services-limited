edbfeabe969ba25d92e8fd7858caaae3c5a37704
Rahul <40555494+Rahulki@users.noreply.github.com>
2025-04-11 21:42:01 -0400

    add docs command in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/c-clear-pool-services-limited/',
+  base:'/c-clear-pool-services-limited/', 
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 })
