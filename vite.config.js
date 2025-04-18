d62165012ff503d2c827a6c598c600e0bbef5b25
Rahul <40555494+Rahulki@users.noreply.github.com>
2025-04-17 22:15:38 -0400

    changed the build base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/c-clear-pool-services-limited/', 
+  base:'/', 
   build: {
     outDir: 'docs',
   },
